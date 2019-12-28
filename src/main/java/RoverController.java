1cc3bc93548bba1a6fe8e649e6bf3a529cfa8631
winter-han1 <541451968@qq.com>
2019-12-28 15:42:18 +0800

    pass

--- src/main/java/RoverController.java
+++ src/main/java/RoverController.java
@@ -7,8 +7,13 @@ public class RoverController {
 
     public String excute(String mission) {
         // 10,10,5,5,E,M,L,M,R
+        rover.land(new Area(10, 10), 5, 5, "E");
 
+        rover.move();
+        rover.turnLeft();
+        rover.move();
+        rover.turnRight();
 
-        return rover.getPosition();
+        return this.rover.getPosition();
     }
 }
